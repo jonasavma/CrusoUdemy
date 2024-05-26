2b2d69e2f6c7c93558aa9699ce5c3371dc257048
Jonas Avelino <jonasavma@gmail.com>
2024-05-26 19:47:35 -0300

    aula 105 Criando e mapeando as classes Pessoa e Endereco para associacao & aula 106 Gravando dados no banco do mapeamento Pessoa e Endereco

---
+++ Neri/hibernate_associacoes/src/oneToOne_unidirecional/Endereco.java
@@ -0,0 +1,24 @@
+package oneToOne_unidirecional;
+
+public class Endereco {
+    private int id_endereco;
+    private String desc_endereco;
+
+    public int getId_endereco() {
+        return id_endereco;
+    }
+
+    public void setId_endereco(int id_endereco) {
+        this.id_endereco = id_endereco;
+    }
+
+    public String getDesc_endereco() {
+        return desc_endereco;
+    }
+
+    public void setDesc_endereco(String desc_endereco) {
+        this.desc_endereco = desc_endereco;
+    }
+    
+    
+}
---
+++ Neri/hibernate_associacoes/src/oneToOne_unidirecional/Pessoa.java
@@ -0,0 +1,26 @@
+package oneToOne_unidirecional;
+public class Pessoa {
+    
+    
+    private int id_pessoa;
+    private String nome_pessoa;
+
+    public int getId_pessoa() {
+        return id_pessoa;
+    }
+
+    public void setId_pessoa(int id_pessoa) {
+        this.id_pessoa = id_pessoa;
+    }
+
+    public String getNome_pessoa() {
+        return nome_pessoa;
+    }
+
+    public void setNome_pessoa(String nome_pessoa) {
+        this.nome_pessoa = nome_pessoa;
+    }
+    
+    
+    
+}
