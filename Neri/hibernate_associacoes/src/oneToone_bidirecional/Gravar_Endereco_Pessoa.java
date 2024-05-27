ee6768181d329c03b7033036c7933507a8b0a992
Jonas Avelino <jonasavma@gmail.com>
2024-05-26 21:50:09 -0300

    aula 110 Associação Bidirecional OneToOne entre Pessoa para Endereco & aula 111 Cascade Save Update na associação OneToOne

---
+++ Neri/hibernate_associacoes/src/oneToone_bidirecional/Gravar_Endereco_Pessoa.java
@@ -0,0 +1,38 @@
+package oneToone_bidirecional;
+
+import javax.swing.JOptionPane;
+import org.hibernate.*;
+import org.hibernate.cfg.*;
+
+public class Gravar_Endereco_Pessoa {
+
+    public static void main(String[] args) {
+
+        try {
+            SessionFactory fabrica = new Configuration().configure().buildSessionFactory();
+            Session sessao = fabrica.openSession();
+            Transaction tx_pessoa = sessao.beginTransaction();
+            Pessoa obj_pessoa = new Pessoa();
+
+            obj_pessoa.setId_pessoa(9);
+            obj_pessoa.setNome_pessoa("Jonas ");
+
+            Endereco obj_endereco = new Endereco();
+            obj_endereco.setId_endereco(obj_pessoa.getId_pessoa());
+            obj_endereco.setDesc_endereco("Rua Salvador Lombone");
+            obj_pessoa.setEndereco(obj_endereco);
+            //sessao.save(obj_endereco);
+
+            tx_pessoa.commit();
+            sessao.close();
+
+            JOptionPane.showMessageDialog(null, "Gravado com Sucesso :");
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro de inserssão :" + e);
+
+        }
+
+    }
+
+}
