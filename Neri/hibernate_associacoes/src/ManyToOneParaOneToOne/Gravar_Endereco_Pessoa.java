f94141cc695df7d84dbdded3ea137ff8f8540dc6
Jonas Avelino <jonasavma@gmail.com>
2024-05-28 00:10:26 -0300

    aula 117 One to one através de UNIQUE true em Foreign Key & aula 118 One to one através Primary Key no hibernate

---
+++ Neri/hibernate_associacoes/src/ManyToOneParaOneToOne/Gravar_Endereco_Pessoa.java
@@ -0,0 +1,42 @@
+package ManyToOneParaOneToOne;
+
+import javax.swing.JOptionPane;
+import org.hibernate.*;
+import org.hibernate.cfg.*;
+
+public class Gravar_Endereco_Pessoa {
+
+    public static void main(String[] args) {
+
+        try {
+            SessionFactory fabrica = new Configuration().configure().buildSessionFactory();
+            Session sessao = fabrica.openSession();
+            Transaction tx_pessoa = sessao.beginTransaction();
+            
+            
+            Endereco obj_endereco = new Endereco();
+            obj_endereco.setId_endereco(1);
+            obj_endereco.setDesc_endereco("Rua Salvador Lombone nº ");
+
+            
+            
+            Pessoa obj_pessoa = new Pessoa();
+            obj_pessoa.setId_pessoa(2);
+            obj_pessoa.setNome_pessoa("Jonas");
+            obj_pessoa.setEndereco(obj_endereco);
+
+            sessao.saveOrUpdate(obj_pessoa);
+
+            tx_pessoa.commit();
+            sessao.close();
+
+            JOptionPane.showMessageDialog(null, "Gravado com Sucesso :");
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro de inserssão :" + e);
+
+        }
+
+    }
+
+}
