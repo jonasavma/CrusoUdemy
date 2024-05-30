959346c8fe314b407b0265fcf77e06ce043d8b98
Jonas Avelino <jonasavma@gmail.com>
2024-05-29 23:44:33 -0300

    aula 122 Mapeando associação One to Many com Set

---
+++ Neri/hibernate_associacoes/src/oneTomany_unidirecional/Endereco.java
@@ -0,0 +1,26 @@
+package oneTomany_unidirecional;
+
+import oneToOne_unidirecional.*;
+
+public class Endereco {
+    private int id_endereco;
+    private String desc_endereco;
+
+    public int getId_endereco() {
+        return id_endereco;
+    }
+
+    public void setId_endereco(int id_endereco) {
+        this.id_endereco = id_endereco;
+    }
+
+    public String getDesc_endereco() {
+        return desc_endereco;
+    }
+
+    public void setDesc_endereco(String desc_endereco) {
+        this.desc_endereco = desc_endereco;
+    }
+    
+    
+}
