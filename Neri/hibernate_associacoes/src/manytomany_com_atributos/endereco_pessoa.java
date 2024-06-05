0af5767aaeeb14ecba9e6c71cc38996e8326df2a
Jonas Avelino <jonasavma@gmail.com>
2024-06-05 00:05:27 -0300

    aula 128 CompositeID associacoes com atrributos & aula 130 Finalizando Composite id associacoes com atributos

---
+++ Neri/hibernate_associacoes/src/manytomany_com_atributos/endereco_pessoa.java
@@ -0,0 +1,28 @@
+package manytomany_com_atributos;
+public class endereco_pessoa {
+    
+    
+    
+    private ids_endereco_pessoa compositeID;
+    private String complemento;
+
+    public ids_endereco_pessoa getCompositeID() {
+        return compositeID;
+    }
+
+    public void setCompositeID(ids_endereco_pessoa compositeID) {
+        this.compositeID = compositeID;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+    
+    
+    
+    
+}
---
+++ Neri/hibernate_associacoes/src/manytomany_com_atributos/ids_endereco_pessoa.java
@@ -0,0 +1,27 @@
+package manytomany_com_atributos;
+
+public class ids_endereco_pessoa {
+
+private Pessoa pessoa;
+private Endereco endereco;
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+
+
+    
+}
