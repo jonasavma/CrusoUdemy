f9b702683d8c5f48c5506f95b0fa299b7159b08e
Jonas Avelino <jonasavma@gmail.com>
2024-06-07 21:40:20 -0300

    aula 138 One to One Bidirecional com tabela ssociativa

---
+++ Neri/hibernate_associacoes/src/onetoone_bidirecional_tabela_associativa/Pessoa.java
@@ -0,0 +1,38 @@
+package onetoone_bidirecional_tabela_associativa;
+
+public class Pessoa {
+    
+    
+    private int id_pessoa;
+    private String nome_pessoa;
+    private Endereco endereco;
+
+    
+    
+    public int getId_pessoa() {
+        return id_pessoa;
+    }
+
+    public void setId_pessoa(int id_pessoa) {
+        this.id_pessoa = id_pessoa;
+    }
+
+    public String getNome_pessoa() {
+        return nome_pessoa;
+    }
+
+    public void setNome_pessoa(String nome_pessoa) {
+        this.nome_pessoa = nome_pessoa;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+    
+    
+    
+}
